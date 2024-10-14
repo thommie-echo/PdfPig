dbe620dd522c89a70bb0a5f8e8516d199359e5db
thommie-echo <troos_1@hotmail.com>
2024-10-14 12:24:58 +0100

    Add test document and character test

---
+++ src/UglyToad.PdfPig.Tests/CharacterTest.cs
@@ -0,0 +1,36 @@
+﻿namespace UglyToad.PdfPig.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using UglyToad.PdfPig.Core;
+    using UglyToad.PdfPig.Logging;
+    using Xunit;
+
+    public class CharacterTest
+    {
+
+        [Fact]
+        public void CorrectlyWritesOperations()
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Schiphol HY.pdf");
+
+            using (PdfDocument document = PdfDocument.Open(path))
+            {
+                foreach (Content.Page page in document.GetPages())
+                {
+                    foreach(Content.Letter letter in page.Letters)
+                    {
+                        if (letter.Value.Contains("fi"))
+                        {
+                            Debug.WriteLine(letter.Value);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
